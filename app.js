0dba7de06ba2b34ceada3faf38c3cea1e3fdcd17
sudeeppatgar <patgarsudeep430@gmail.com>
2026-02-04 08:53:10 +0530

    added helmet

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
+// import routes from "./routes/index.js";
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(helmet());
 // app.use("/", routes);
 app.get("/", (req, res) => {
   res.send("hello world");
