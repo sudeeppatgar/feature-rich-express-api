f679b3c1025b56ad117cbaf49890bce8a05eb6d1
sudeeppatgar <patgarsudeep430@gmail.com>
2026-02-15 17:12:11 +0530

    removed comented code

--- middlewares/async.middleware.js
+++ middlewares/async.middleware.js
@@ -1,9 +1,3 @@
 export const asyncHandler = (requestHandler) => (req, res, next) => {
   Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
 };
-
-// export const asyncHandler2 = (requestHandler) => {
-//   return (req, res, next) => {
-//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-//   };
-// };
