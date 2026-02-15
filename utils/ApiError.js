a88fddc58a0c60cbbedee2d5ffa159e04333ebd1
sudeeppatgar <patgarsudeep430@gmail.com>
2026-02-15 15:39:03 +0530

    modified

--- utils/ApiError.js
+++ utils/ApiError.js
@@ -1,12 +1,12 @@
 class ApiError extends errors {
   constructor(
-    statuscode,
+    statusCode,
     message = "somthing went wrong",
     errors = [],
     stack = "",
   ) {
     super(message);
-    this.statuscode = statuscode;
+    this.statusCode = statusCode;
     this.data = null;
     this.message = message;
     this.success = false;
